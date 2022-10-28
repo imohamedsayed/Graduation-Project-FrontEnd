bc0e541bfb7c84003b0c6ff5fa01ea0830b4581d
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2022-10-28 14:12:05 +0200

    create new course

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CreateCourse from '../views/Create_new_course.vue'
 
 const routes = [
   {
@@ -7,7 +8,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
- 
+  {
+    path: '/new_cours',
+    name: 'new_cours',
+    component: CreateCourse
+  },
 ]
 
 const router = createRouter({
