265fe98ee77303171305f1c754c46f18e6734243
ahmed sameh <as6864886@gmail.com>
2023-06-17 15:51:09 +0200

    cate

---
+++ src/components/website/store/category.vue
@@ -0,0 +1,19 @@
+<template>
+                  <div class="row">
+                    <router-link class=" d-grid col-6 mx-auto" :to="{ name:'websiteStore'}">
+                        <button type="button" class="btn btn-primary btn-lg">ممنوعات  ي صحبي</button>
+      </router-link>
+                  </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+
+    },
+  };
+  </script>
+  
+  <style lang="scss">
+</style>
+  
